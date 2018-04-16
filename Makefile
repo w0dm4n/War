62fc9435d3ea456d06f952774e31be1a66fb33c3
jguyet <gygydu19@hotmail.fr>
2018-04-16 19:01:58 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,34 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: frmarinh <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/03/07 12:48:21 by frmarinh          #+#    #+#              #
+#    Updated: 2017/03/07 15:00:34 by frmarinh         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = War
+
+SRCS =	\
+		srcs/main.c					\
+		
+O_FILE = $(SRCS:.c=.o)
+
+FLAGS = -mwindows -ggdb
+
+#FLAGS = -Wall -Wextra -Werror
+
+all: $(NAME)
+
+$(NAME):
+	@gcc $(SRCS) $(FLAGS) -I includes/ -o $(NAME)
+
+clean:
+	@del $(NAME).exe
+
+fclean: clean
+
+re:		fclean all
---
+++ srcs/main.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frmarinh <frmarinh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/01 18:05:22 by frmarinh          #+#    #+#             */
+/*   Updated: 2017/07/01 18:07:34 by frmarinh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "all.h"
+
+/*
+**	Hide and close the current terminal
+*/
+void				close_console()
+{
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_MINIMIZE);
+	ShowWindow(hWnd, SW_HIDE);
+	FreeConsole();
+}
+
+/*
+**	Start a new console and relink file descriptor 0, 1, 2
+**	then resizing window 300x600 to position x(100) y(100) and disabling resizing
+*/
+void				start_debug_console()
+{
+	AllocConsole();
+	freopen("CONIN$", "r", stdin);
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONOUT$", "w", stderr);
+	HWND hWnd = GetConsoleWindow();
+	if (hWnd) {
+		SetWindowPos(hWnd, 0, 100, 100, 600, 600, NO_FLAGS);
+		SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) &~ WS_SIZEBOX);
+	}
+}
+
+/*
+** Just in case a SEGV is knocking...
+*/
+void signal_handler(int signal)
+{
+	printf("Something went wrong, i'm dying... good bye\n");
+	exit (1);
+}
+
+/*
+**	Main entry point
+*/
+int					main(int argc, char **argv)
+{
+	setlocale(LC_CTYPE, "");
+	signal(SIGSEGV, signal_handler);
+	return (0);
+}
